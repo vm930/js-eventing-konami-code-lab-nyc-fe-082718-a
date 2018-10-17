f778af7e8d3a9ec3fb0fbb6cc89204c7b6686d79
abstract-animation-9914 <vm930@hotmail.com>
2018-10-17 13:37:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
+init();
+
 function init() {
   // Write your JavaScript code inside the init() function
 document.body.addEventListener("keydown", keydownHandler)
